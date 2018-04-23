635ca4a3f2a49394699cda16144f451a49a377ee
Doan Manh Hung <hungdmbrse@gmail.com>
2018-04-23 16:09:14 +0900

    課題１９完了しました

---
+++ Question19/app/src/main/java/com/example/mackanrishastv/question19/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mackanrishastv.question19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+
+    MyRecyclerViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+//        String[] data = {"1", "2", "3", "4", "5", "6", "7", "8"};
+        int[] lstImg = {R.drawable.fb, R.drawable.go, R.drawable.inst, R.drawable.wo, R.drawable.twiter, R.drawable.you};
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvNumbers);
+        int numberOfColumns = 2;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        adapter = new MyRecyclerViewAdapter(this, lstImg);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
+    }
+}
+
