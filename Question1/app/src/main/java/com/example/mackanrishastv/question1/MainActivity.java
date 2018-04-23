0b75489e8a3d9d54bb9939201abba055d9f8c18e
Doan Manh Hung <hungdmbrse@gmail.com>
2018-04-23 16:32:09 +0900

    課題１アップデートした

--- Question1/app/src/main/java/com/example/mackanrishastv/question1/MainActivity.java
+++ Question1/app/src/main/java/com/example/mackanrishastv/question1/MainActivity.java
@@ -11,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        boolean isTrue = true;
+        String str = "Question 1";
+        int q1 = 24;
+        double douQ1 = 24.26d;
+
+        Log.d("Question1", String.valueOf(isTrue));
+        Log.d("Question1", str);
+        Log.d("Question1", String.valueOf(q1));
+        Log.d("Question1", String.valueOf(douQ1));
+
+
     }
 }
