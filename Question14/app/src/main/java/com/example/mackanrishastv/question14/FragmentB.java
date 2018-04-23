a10638cce7a8904dd9e101f30e78d8ad53ee5b90
Doan Manh Hung <hungdmbrse@gmail.com>
2018-04-23 16:03:47 +0900

    課題１４完了しました

---
+++ Question14/app/src/main/java/com/example/mackanrishastv/question14/FragmentB.java
@@ -0,0 +1,19 @@
+package com.example.mackanrishastv.question14;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class FragmentB extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmen_b, container, false);
+    }
+}
