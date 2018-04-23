89c2396d2bea3da3111abc8e460724423887f253
Doan Manh Hung <hungdmbrse@gmail.com>
2018-04-23 15:10:22 +0900

    課題１完了しました

--- Question1/app/src/main/java/com/example/mackanrishastv/question1/MainActivity.java
+++ Question1/app/src/main/java/com/example/mackanrishastv/question1/MainActivity.java
@@ -11,18 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        boolean bool = true;
-        boolean ibool = false;
-
-        Log.d("ManhHung", String.valueOf(bool) + " - " + String.valueOf(ibool));
-
-        int temp = 24;
-        double temp1 = 24.654d;
-        String temp2 = "研修";
-
-        Log.d("ManhHung", "Int = " + temp);
-        Log.d("ManhHung", "Double = " + temp1);
-        Log.d("ManhHung", "String = " + temp2);
-
     }
 }
