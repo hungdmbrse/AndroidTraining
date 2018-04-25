6df9a56e77f9547319768ec5df7a8e4ffcd60925
Doan Manh Hung <hungdmbrse@gmail.com>
2018-04-25 09:14:07 +0900

    課題７やり直しました

--- question7/app/src/main/java/com/example/mackanrishastv/question7/MainActivity.java
+++ question7/app/src/main/java/com/example/mackanrishastv/question7/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mackanrishastv.question7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Math math = new Math();
+        math.calculate(new Result() {
+            @Override
+            public void result(int resultValue) {
+                Log.d("Callback", String.valueOf(resultValue));
+            }
+        });
+
     }
 }
--- question7/app/src/main/java/com/example/mackanrishastv/question7/Math.java
+++ question7/app/src/main/java/com/example/mackanrishastv/question7/Math.java
@@ -2,7 +2,9 @@ package com.example.mackanrishastv.question7;
 
 public class Math {
 
-    public int calculate(int a, int b){
-        return a+b;
+    public void calculate(Result result){
+        int a = 5;
+        int b = 3;
+        result.result(a+b);
     }
 }
