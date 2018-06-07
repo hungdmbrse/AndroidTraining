ec6a4e9e6848c822f1cdf33d030f0e590a063552
Doan Manh Hung <hungdmbrse@gmail.com>
2018-06-07 10:34:33 +0900

    課題２５修正いたしました。

---
+++ question255/app/src/main/java/com/example/mackanrishastv/question25/TodoListAdapter.java
@@ -0,0 +1,64 @@
+package com.example.mackanrishastv.question25;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class TodoListAdapter extends BaseAdapter {
+    private Context context;
+    private int layout;
+    private List<Todo> arrayListTodo;
+
+    public TodoListAdapter(Context context, int layout, List<Todo> arrayListTodo) {
+        this.context = context;
+        this.layout = layout;
+        this.arrayListTodo = arrayListTodo;
+    }
+
+    public void updateReceiptsList(List<Todo> newlist) {
+        this.arrayListTodo = newlist;
+        this.notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return arrayListTodo.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = layoutInflater.inflate(layout, null);
+
+        TextView txtViewTodoTitle = (TextView) convertView.findViewById(R.id.txtViewTodoTitle);
+        TextView txtViewTodoContents = (TextView) convertView.findViewById(R.id.txtViewTotoContent);
+        TextView txtViewTodoCreated = (TextView) convertView.findViewById(R.id.txtViewTodoCreated);
+        TextView txtViewTodoModified = (TextView) convertView.findViewById(R.id.txtViewTodoModified);
+        TextView txtViewTodoLimitDate = (TextView) convertView.findViewById(R.id.txtViewTodoLimitDate);
+
+        Todo todo = arrayListTodo.get(position);
+
+        txtViewTodoTitle.setText(todo.getTodo_title());
+        txtViewTodoContents.setText(todo.getTodo_contents());
+        txtViewTodoCreated.setText(todo.getCreated());
+        txtViewTodoModified.setText(todo.getModified());
+        txtViewTodoLimitDate.setText(todo.getLimit_date());
+
+        return  convertView;
+    }
+}
